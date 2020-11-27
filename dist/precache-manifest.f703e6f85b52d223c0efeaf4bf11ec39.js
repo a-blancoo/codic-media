a90d65b175720466789ef103a12aef8a01264afa
a-blancoo <angel.skolan@gmail.com>
2020-11-27 18:17:29 +0100

    add-dist

---
+++ dist/precache-manifest.f703e6f85b52d223c0efeaf4bf11ec39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcfcc70c5ecc927edb2",
+    "url": "/css/app.bebb6104.css"
+  },
+  {
+    "revision": "ef254fb141b3147bf0bb786b1bcf3656",
+    "url": "/img/logo.ef254fb1.svg"
+  },
+  {
+    "revision": "15da0abd78d4d7cb725d0771bef80c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adcfcc70c5ecc927edb2",
+    "url": "/js/app.3db3abbd.js"
+  },
+  {
+    "revision": "d0465b994a95d664e022",
+    "url": "/js/chunk-vendors.67f7360f.js"
+  },
+  {
+    "revision": "db31bf4807f47c668280a84a26a5bfcf",
+    "url": "/manifest.json"
+  }
+]);
